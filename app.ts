efd9eee557da8207b6c54aaccbf169394ff3f0dd
Edgar Fabian Gonzalez Peña <edgargonzalez.developer@gmail.com>
2018-04-02 11:38:10 +0100

    Destructuring and Spread operator has been implemented

--- app.ts
+++ app.ts
@@ -2,3 +2,54 @@ import { Category } from './enums';
 import { Book, Logger, Author, Librarian, Magazine } from './interfaces';
 import { UniversityLibrarian, ReferenceItem } from './classes';
 import * as util from './lib/utilityFunctions';
+
+//function PrintBookInfo({title: booktitle, author: bookauthor} : Book): void {
+ function PrintBookInfo(item : Book): void {
+    // console.log(`${item.title} was authored by ${item.author}`);
+    console.log(`${item.title} was authored by ${item.author}`);
+
+}
+
+function LogFavouriteBooks([book1, book2, ...others]: Array<Book>){
+    PrintBookInfo(book1);
+    PrintBookInfo(book2);
+    others.forEach(book => console.log(book));
+}
+
+
+
+
+/***************************************************************************************** */
+//Destructuring Arrays
+let [book1, book2] = util.GetAllBooks();
+//PrintBookInfo(book1);
+// PrintBookInfo(book2);
+
+//LogFavouriteBooks(util.GetAllBooks());
+//Destructuring Objects
+let { title, author } = book1;
+// console.log(title);
+// console.log(author);
+
+//Destructuring Objects option 2
+let { title: booktitle, author: bookauthor } = book1;
+// console.log(booktitle);
+// console.log(bookauthor);
+
+
+let schoolBooks: Array<Book> = [
+    { id: 1, title: 'The Great Gatsby', author: 'James Royce', available: true, category: Category.Biography },
+    { id: 2, title: 'Crime and Punishment', author: 'Ernest Hemingway', available: false, category: Category.Poetry },
+    { id: 3, title: 'Clear light of Day ', author: 'Maya Angelou', available: true, category: Category.Biography }
+];
+
+let booksRead : Array<Book> = util.GetAllBooks();
+//Spread operator
+booksRead.push(...schoolBooks);
+// booksRead.forEach(book => console.log(book));
+
+let poets:Array<string> = ['Shelley', 'Collins', 'hughes'];
+let authors: string [] = ['Tolstoy', 'Fitzgerald', ...poets];
+authors.forEach(book => console.log(book));
+
+
--- js/app.js
+++ js/app.js
@@ -1,3 +1,42 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var enums_1 = require("./enums");
+var util = require("./lib/utilityFunctions");
+//function PrintBookInfo({title: booktitle, author: bookauthor} : Book): void {
+function PrintBookInfo(item) {
+    // console.log(`${item.title} was authored by ${item.author}`);
+    console.log(item.title + " was authored by " + item.author);
+}
+function LogFavouriteBooks(_a) {
+    var book1 = _a[0], book2 = _a[1], others = _a.slice(2);
+    PrintBookInfo(book1);
+    PrintBookInfo(book2);
+    others.forEach(function (book) { return console.log(book); });
+}
+/***************************************************************************************** */
+//Destructuring Arrays
+var _a = util.GetAllBooks(), book1 = _a[0], book2 = _a[1];
+//PrintBookInfo(book1);
+// PrintBookInfo(book2);
+//LogFavouriteBooks(util.GetAllBooks());
+//Destructuring Objects
+var title = book1.title, author = book1.author;
+// console.log(title);
+// console.log(author);
+//Destructuring Objects option 2
+var booktitle = book1.title, bookauthor = book1.author;
+// console.log(booktitle);
+// console.log(bookauthor);
+var schoolBooks = [
+    { id: 1, title: 'The Great Gatsby', author: 'James Royce', available: true, category: enums_1.Category.Biography },
+    { id: 2, title: 'Crime and Punishment', author: 'Ernest Hemingway', available: false, category: enums_1.Category.Poetry },
+    { id: 3, title: 'Clear light of Day ', author: 'Maya Angelou', available: true, category: enums_1.Category.Biography }
+];
+var booksRead = util.GetAllBooks();
+//Spread operator
+booksRead.push.apply(booksRead, schoolBooks);
+// booksRead.forEach(book => console.log(book));
+var poets = ['Shelley', 'Collins', 'hughes'];
+var authors = ['Tolstoy', 'Fitzgerald'].concat(poets);
+authors.forEach(function (book) { return console.log(book); });
 //# sourceMappingURL=app.js.map
